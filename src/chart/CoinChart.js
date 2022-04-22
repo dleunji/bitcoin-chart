a69d4ebba83f551cb53b733d58d59f8e9941fc47
Lee Eunji <lej970703@naver.com>
2022-04-22 13:56:15 +0900

    :recycle: Refactoring

--- src/chart/CoinChart.js
+++ src/chart/CoinChart.js
@@ -1,69 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { init, dispose } from 'klinecharts';
+import React, { useState, useEffect } from "react";
+import { init, dispose } from "klinecharts";
 
-import Layout from '../Layout';
-import useNewData from '../hooks/useNewData';
-import getInitialDataList from '../utils/getInitialDataList';
-import getLanguageOption from '../utils/getLanguageOption';
+import Layout from "../Layout";
+import useNewData from "../hooks/useNewData";
+import getInitialDataList from "../utils/getInitialDataList";
+import getLanguageOption from "../utils/getLanguageOption";
 
 const types = [
-  { key: 'candle_solid', text: '캔들' },
-  { key: 'candle_stroke', text: '투명 캔들' },
-  { key: 'ohlc', text: 'Bar 형식의 OHLC' },
-  { key: 'area', text: 'Mountain' }
+  { key: "candle_solid", text: "캔들" },
+  { key: "candle_stroke", text: "투명 캔들" },
+  { key: "ohlc", text: "Bar 형식의 OHLC" },
+  { key: "area", text: "Mountain" },
 ];
 const CoinChart = () => {
   let chart;
   const [initialized, setInitialized] = useState(false);
   const newData = useNewData();
 
-  useEffect(()=>{
-    chart = init('coin-chart');
+  useEffect(() => {
+    chart = init("coin-chart");
     chart.setStyleOptions(getLanguageOption());
     const fetchData = async () => {
       const dataList = await getInitialDataList(1);
       chart.applyNewData(dataList);
       setInitialized(true);
-    }
+    };
     fetchData();
-    return() => {
-      dispose('chart');
-    }
-  },[]);
+    return () => {
+      dispose("chart");
+    };
+  }, []);
 
-  useEffect(()=>{
-    // TODO: init이 아니라 기존에 존재하는 차트 불러오기 필요
-    chart = init('coin-chart');
-    if(initialized){
+  useEffect(() => {
+    chart = init("coin-chart");
+    if (initialized) {
       chart.updateData(newData);
     }
   }, [newData]);
 
-  return(
+  return (
     <Layout title="Bitcoin(BTC-KRW) 실시간 가격 조회">
-      <div id="coin-chart" className="coin-chart"/>
-      <div
-        className="chart-menu-container">
-        {
-          types.map(({ key, text }) => {
-            return (
-              <button
-                key={key}
-                onClick = {_ => {
-                  chart.setStyleOptions({
-                    candle: {
-                      type: key
-                    }
-                  })
-                }}>
-                {text}
-              </button>
-            )
-          })
-        }
+      <div id="coin-chart" className="coin-chart" />
+      <div className="chart-menu-container">
+        {types.map(({ key, text }) => {
+          return (
+            <button
+              key={key}
+              onClick={(_) => {
+                chart.setStyleOptions({
+                  candle: {
+                    type: key,
+                  },
+                });
+              }}
+            >
+              {text}
+            </button>
+          );
+        })}
       </div>
     </Layout>
-  )
-}
+  );
+};
 
 export default CoinChart;
--- src/hooks/useNewData.js
+++ src/hooks/useNewData.js
@@ -1,35 +1,46 @@
-import React, { useState, useEffect, useRef } from 'react';
-
+import { useState, useEffect, useRef } from "react";
+const UPBIT_URL = "wss://api.upbit.com/websocket/v1";
 const useNewData = () => {
   const [result, setResult] = useState();
   const [timer, setTimer] = useState(false);
-  const data = [{'ticket':'nexoneunji'},{'type':'ticker','codes':['KRW-BTC'], 'isOnlyRealtime':true}];
+  const data = [
+    { ticket: "nexoneunji" },
+    { type: "ticker", codes: ["KRW-BTC"], isOnlyRealtime: true },
+  ];
   const ws = useRef(null);
 
-  useEffect(()=>{
-    if(timer == true){
-      alert('만료되었습니다.');
+  useEffect(() => {
+    // timer 종료 시 트리거
+    if (timer) {
+      alert("만료되었습니다.");
       ws.current.close();
     }
   }, [timer]);
 
-  useEffect(()=>{
+  useEffect(() => {
     // 10분 지나면 종료 처리
-    setTimeout(()=>{
+    setTimeout(() => {
       setTimer(true);
     }, 10 * 60 * 1000);
 
-    ws.current = new WebSocket('wss://api.upbit.com/websocket/v1');
+    ws.current = new WebSocket(UPBIT_URL);
     ws.current.onopen = () => {
       ws.current.send(JSON.stringify(data));
-    }
+    };
     ws.current.onclose = () => {
-      console.log('DISCONNECTED');
-    }
+      console.log("DISCONNECTED");
+    };
     ws.current.onmessage = async (event) => {
       const text = await new Response(event.data).text();
       const message = JSON.parse(text);
-      const {opening_price, low_price, high_price, trade_price, timestamp, trade_volume} = message;
+      const {
+        opening_price,
+        low_price,
+        high_price,
+        trade_price,
+        timestamp,
+        trade_volume,
+      } = message;
       setResult({
         open: opening_price,
         low: low_price,
@@ -38,19 +49,22 @@ const useNewData = () => {
         close: trade_price,
         volume: trade_volume,
         // 오전 9시 기준 일봉
-        timestamp : Math.floor(timestamp / 24 / 60 / 60 / 1000) * 24 * 60 * 60 * 1000,
-        turnover: (opening_price + low_price + high_price + trade_price) / 4 * trade_volume
+        timestamp:
+          Math.floor(timestamp / 24 / 60 / 60 / 1000) * 24 * 60 * 60 * 1000,
+        turnover:
+          ((opening_price + low_price + high_price + trade_price) / 4) *
+          trade_volume,
       });
-    }
-    ws.current.onerror = (event)=>{
-      console.log('Error',event);
+    };
+    ws.current.onerror = (event) => {
+      console.log("Error", event);
       ws.current.close();
-    }
+    };
     return () => {
       ws.current.close();
-    }
-  },[]);
+    };
+  }, []);
   return result;
-}
+};
 
 export default useNewData;
